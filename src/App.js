f7821acbd7c517b4ff81beef5a4943513ea79732
priscilamattos <priiica12@gmail.com>
2022-05-08 22:36:49 -0400

    Fix: fetch moved to another document

--- src/App.js
+++ src/App.js
@@ -1,52 +1,23 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 //Components
-import Movie from "./components/Movie";
+import Movies from "./components/Movies";
 import SearchBar from "./components/SearchBar";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { UserContextProvider } from "./context";
 
 //Style
-
 import "./styles/App.scss";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    //grab information from moviedb API
-    //store our keys
-    //search for movies
-    const fetchData = async () => {
-      const response = await axios({
-        method: "GET",
-        url: "https://api.themoviedb.org/3/trending/all/day?",
-        params: {
-          format: "json",
-          api_key: "9279e74f93d44d00c0b5afd5efff4065",
-        },
-      });
-      // TO DO DELETE THIS RESPONSE22 - using just to get image sizes
-      const response22 = await axios({
-        method: "GET",
-        url: "https://api.themoviedb.org/3/configuration?api_key=9279e74f93d44d00c0b5afd5efff4065",
-      });
-      setMovies(response.data.results);
-      console.log("=====", response22);
-    };
-    fetchData();
-  }, []);
-
   return (
     <div className="App">
-      <Header />
-      <SearchBar />
-      <main className="wrapper container">
-        {movies.map((movie) => {
-          return <Movie movie={movie} key={movie.id} />;
-        })}
-      </main>
+      <UserContextProvider>
+        <Header />
+        <SearchBar />
+        <main className="wrapper container">
+          <Movies />
+        </main>
+      </UserContextProvider>
       <Footer />
     </div>
   );
