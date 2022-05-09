1a65feb5a25a1aa97ab8395bcd62a4bb59ff819f
TheoMitchell <theomitchell@gmail.com>
2022-05-08 20:02:29 -0400

    TDM1. Initial setup Firebase connection to project. Console.log data from DB.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+//Modules
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+//Firebase DB
+import firebase from "./scripts/firebase";
+import { getDatabase, ref, onValue } from "firebase/database";
+
 //Components
+import Header from "./components/Header";
 import Movies from "./components/Movies";
+import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import { UserContextProvider } from "./context";
 
@@ -9,6 +18,21 @@ import { UserContextProvider } from "./context";
 import "./styles/App.scss";
 
 const App = () => {
+  //States
+  // const [movies, setMovies] = useState([]);
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    // Variable to hold the DB
+    const database = getDatabase(firebase);
+    // Variables to store the references of the DB noves
+    const dbRef = ref(database);
+    // Grab info from a specific point (reference) in the DB
+    onValue(dbRef, (response) => {
+      console.log(response.val());
+    });
+  });
+
   return (
     <div className="App">
       <UserContextProvider>
