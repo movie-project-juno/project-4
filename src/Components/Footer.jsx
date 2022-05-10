fcaf9de53c93e77853d0e203b5ff538a86990758
priscilamattos <priiica12@gmail.com>
2022-05-10 12:35:55 -0400

    Style for footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
   return (
     <footer>
       <div className="wrapper">
-        <h5>Juno College 2022</h5>
+        <h5>Juno College | Cohort 40 | 2022</h5>
+        <p>
+          Created by: Alex Rodgers | Priscila Mattos | Sophia Kim | Theo
+          Mitchell
+        </p>
       </div>
     </footer>
   );
