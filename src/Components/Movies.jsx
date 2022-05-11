18a5c7f1f468cec002b8d1129965acd3a54b86dc
priscilamattos <priiica12@gmail.com>
2022-05-10 23:47:23 -0400

    File renamed.

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 
-import { UserContext } from "../context";
+import { UserContext } from "../context/apiMovies";
 import MovieModal from "./MovieModal";
 
 //Imporrting movies from context
